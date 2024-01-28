0ef691850b562af11cada3755f85237ed68c638c
shimagit <shimasakezuki@icloud.com>
2024-01-28 11:35:34 +0900

    all finish

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ var gImg;
 var gScreen;
 var gTimer;
 
+class Status
+{
+  static sScore = 0;
+
+  static Draw( g )
+  {
+    g.font = "32px monospace";
+    g.fillStyle = "#ffffff";
+    g.fillText( "SCORE " + Status.sScore, 0, 32 );
+
+  }
+}
+
 class Character
 {
   constructor( idx )
@@ -68,7 +81,7 @@ class CharaMgr
   searchInCircle( x, y, s )
   {
     for( let o of this.mList ){
-      if( !o.mVisible ){
+      if( !o.mVisible || o.mCnt ){
         continue;
       }
       if( o.isInCircle( x, y, s )){
@@ -105,8 +118,12 @@ class Bullet extends Character
     super( 3 );
   }
 
-  static Appear( x, y )
+  static Appear( x, y, typ = 3 )
   {
+    if( typ == 4 ){
+      Bullet.GetInstance().appear( x, y, Math.atan2( Player.sObj.mY - y, Player.sObj.mX - x ), 4, 2 );
+      return;
+    }
     for( let i = 0; i < 32; i++){
       Bullet.GetInstance().appear( x, y, Math.PI * i / 16 );
     }
@@ -137,12 +154,13 @@ class Bullet extends Character
     Bullet.sMgr.tick();
   }
 
-  appear( x, y, a )
+  appear( x, y, a, idx = 3, spd = 1 )
   {
     this.mX = x;
     this.mY = y;
-    this.mDX = Math.cos( a );
-    this.mDY = Math.sin( a );
+    this.mDX = Math.cos( a ) * spd;
+    this.mDY = Math.sin( a ) * spd;
+    this.mIdx = idx;
     this.mVisible = true;
   }
 }
@@ -160,15 +178,16 @@ class Enemy extends Character
     this.mDX = 0;
     this.mDY = 1;
   }
-
+  
   static Appear()
   {
     let o = Enemy.GetInstance();
-
+    
     o.mX = Math.floor( Math.random() * WIDTH ) - 8;
     o.mY = -15;
     o.mDX = 0;
     o.mDY = 1;
+    o.mIdx = 1;
     o.mVisible = true;
   }
 
@@ -198,6 +217,34 @@ class Enemy extends Character
     if( Enemy.sCount++ > 20){
       Enemy.sCount = 0;
       Enemy.Appear();
+    }
+  }
+
+  tick()
+  {
+    if( !this.mVisible ){
+      return;
+    }
+
+    super.tick();
+    
+    if( Math.random() < 0.01 ){
+      Bullet.Appear( this.mX, this.mY, 4 );
+    }
+
+    if( this.mCnt == 0 ){
+      return;
+    }
+
+    if( --this.mCnt !=0 ){
+      return;
+    }
+
+    if( this.mIdx < 8 ){
+        this.mIdx++;
+        this.mCnt = 4;
+    }else{
+      this.mVisible = false;
     }
   }
 }
@@ -231,12 +278,19 @@ class Missile extends Character
   static Tick()
   {
     let o = Missile.sObj;
+    if( !o.mVisible ){
+      return;
+    }
+
     o.tick();
 
     let e = Enemy.SearchInCircle( o.mX + 8, o.mY + 8, 200 );
     if( e ){
-      e.mVisible = false;
+      e.mIdx = 5;
+      e.mCnt = 8;
+      e.mDY = 0;
       o.mVisible = false;
+      Status.sScore += 100;
       Bullet.Appear( e.mX, e.mY );
     }
   }
@@ -263,10 +317,10 @@ class Player extends Character
   static Tick()
   {
     let o = Player.sObj;
-    if( gKey[ 37 ] ) o.mX -= 2;
-    if( gKey[ 38 ] ) o.mY -= 2;
-    if( gKey[ 39 ] ) o.mX += 2;
-    if( gKey[ 40 ] ) o.mY += 2;
+    if( gKey[ 37 ] && o.mX > 0 ) o.mX -= 2;
+    if( gKey[ 38 ] && o.mY > 0 ) o.mY -= 2;
+    if( gKey[ 39 ] && o.mX < WIDTH  - 16 ) o.mX += 2;
+    if( gKey[ 40 ] && o.mY < HEIGHT - 16 ) o.mY += 2;
     if( gKey[ 90 ] ) Missile.Shot();
 
     let x = o.mX + 8;
@@ -290,6 +344,7 @@ function draw()
   let g = e.getContext("2d");
   g.imageSmoothingEnabled = g.msImageSmoothingEnabled = false;
   g.drawImage( gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, e.width, e.height );
+  Status.Draw( g );
 
   // Player.Draw();
 }
